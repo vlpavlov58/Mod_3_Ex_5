83cf60ce6a8b1ad48634bea9c78c6a6ea0f429e2
Владислав Павлов <vlpavlov58@gmail.com>
2018-02-23 21:34:49 +0200

    Add project files.

---
+++ Mod_3_Ex_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_3_Ex_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
