51e50dac07f5b93de92122e0eb6a3225dd7ba0e2
Владислав Павлов <vlpavlov58@gmail.com>
2018-02-23 22:02:15 +0200

    Write a program that reads from the console two int numbers and prints how many numbers between them exist, such that the reminder of their division by 5 is 0. Example: in the range (14, 25) there are 3 such numbers: 15, 20 and 25.

--- Mod_3_Ex_5/Program.cs
+++ Mod_3_Ex_5/Program.cs
@@ -10,6 +10,21 @@ namespace Mod_3_Ex_5
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter start number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter finish number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i%5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+         
         }
     }
 }
